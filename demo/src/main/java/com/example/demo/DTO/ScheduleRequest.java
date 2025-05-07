dfb08eeac31c48d693369f6403f0f74ba2985401
dnr <rjsdnr1221@naver.com>
2025-05-07 16:35:01 +0900

    일정 등록/조회

--- demo/src/main/java/com/example/demo/DTO/ScheduleRequest.java
+++ demo/src/main/java/com/example/demo/DTO/ScheduleRequest.java
@@ -1,17 +1,2 @@
-package com.example.demo.DTO;
-
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ScheduleRequest {
-
-    private String title;
-    private String description;
-    private LocalDateTime scheduledAt;
-    private Long protectedUserId;
+package com.example.demo.DTO;public class ScheduleRequest {
 }
