3649d7bd0bc514be76b8b33ac61e20ea2f9b93f4
Songjaol <thd011124@gmail.com>
2025-05-07 17:22:31 +0900

    Revert "일정 등록/조회"
    This reverts commit dfb08eeac31c48d693369f6403f0f74ba2985401.

--- demo/src/main/java/com/example/demo/DTO/ScheduleRequest.java
+++ demo/src/main/java/com/example/demo/DTO/ScheduleRequest.java
@@ -1,2 +1,17 @@
-package com.example.demo.DTO;public class ScheduleRequest {
+package com.example.demo.DTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleRequest {
+
+    private String title;
+    private String description;
+    private LocalDateTime scheduledAt;
+    private Long protectedUserId;
 }
