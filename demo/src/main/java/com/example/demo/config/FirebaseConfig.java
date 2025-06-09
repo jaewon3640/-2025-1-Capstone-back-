356a2d46146049daf11953974155cfb6d4375b04
kih056 <207148057+kih056@users.noreply.github.com>
2025-06-10 00:56:50 +0900

    FCM 설정 파일 추가

---
+++ demo/src/main/java/com/example/demo/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
+    private String fileName;
+
+    @PostConstruct
+    public void init() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("demo/src/main/resources/" + fileName);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
